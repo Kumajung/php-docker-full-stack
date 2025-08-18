2edd1821bc3b230cb43401a5a6661b987c09c938
kumajung <kaidohjung@gmail.com>
2025-08-18 15:21:46 +0700

    Use env vars for DB connection

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php
 
-$connect = mysqli_connect(
-    'db', # service name
-    'php_docker', # username
-    'password', # password
-    'php_docker' # db table
-);
+$host = getenv('DB_HOST') ?: 'db';
+$port = getenv('DB_PORT') ?: '3306';
+$user = getenv('DB_USER') ?: 'php_docker';
+$pass = getenv('DB_PASS') ?: 'password';
+$db   = getenv('DB_NAME') ?: 'php_docker';
 
-$table_name = "php_docker_table";
+$connect = mysqli_connect($host, $user, $pass, $db, (int)$port);
+if (!$connect) {
+    die('Connection error: ' . mysqli_connect_error());
+}
 
+$table_name = 'php_docker_table';
 $query = "SELECT * FROM $table_name";
-
 $response = mysqli_query($connect, $query);
 
 echo "<strong>$table_name: </strong>";
-while($i = mysqli_fetch_assoc($response))
-{
-    echo "<p>".$i['title']."</p>";
-    echo "<p>".$i['body']."</p>";
-    echo "<p>".$i['date_created']."</p>";
-    echo "<p>Kumamen</p>";
-    echo "<hr>";
+while ($i = mysqli_fetch_assoc($response)) {
+    echo '<p>'.$i['title'].'</p>';
+    echo '<p>'.$i['body'].'</p>';
+    echo '<p>'.$i['date_created'].'</p>';
+    echo '<p>Kumamen</p>';
+    echo '<hr>';
 }
+
